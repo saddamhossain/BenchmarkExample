9823ffdec544fe3728993e879c00a50113cd12b8
saddamhossain <saddamhossain953@gmail.com>
2023-07-30 15:04:24 +0600

    Add project files.

---
+++ BenchmarkExample/DateParser.cs
@@ -0,0 +1,42 @@
+﻿namespace BenchmarkExample;
+
+// 2019-12-13T16:33:06Z
+public class DateParser
+{
+    public int GetYearFromDateTime(string dateTimeAsString)
+    {
+        var dateTime = DateTime.Parse(dateTimeAsString);
+        return dateTime.Year;
+    }
+
+    public int GetYearFromSplit(string dateTimeAsString)
+    {
+        var splitOnHiphen = dateTimeAsString.Split('-');
+        return int.Parse(splitOnHiphen[0]);
+    }
+
+    public int GetYearFromSubstring(string dateTimeAsString)
+    {
+        var indexOfHiphen = dateTimeAsString.IndexOf('-');
+        return int.Parse(dateTimeAsString.Substring(0, indexOfHiphen));
+    }
+
+    public int GetYearFromSpan(ReadOnlySpan<char> dateTimeSpan)
+    {
+        var indexOfHiphen = dateTimeSpan.IndexOf('-');
+        return int.Parse(dateTimeSpan.Slice(0, indexOfHiphen));
+    }
+
+    public int GetYearFromSpanManualConversion(ReadOnlySpan<char> dateTimeSpan)
+    {
+        var indexOfHiphen = dateTimeSpan.IndexOf('-');
+        var yearAsSpan = dateTimeSpan.Slice(0, indexOfHiphen);
+
+        var temp = 0;
+        for (int i = 0; i < yearAsSpan.Length; i++)
+        {
+            temp = temp * 10 + (yearAsSpan[i] - '0');
+        }
+        return temp;
+    }
+}
---
+++ BenchmarkExample/DateParserBenchmarks.cs
@@ -0,0 +1,43 @@
+﻿using BenchmarkDotNet.Attributes;
+namespace BenchmarkExample;
+
+// https://benchmarkdotnet.org/articles/overview.html
+[MemoryDiagnoser]
+[Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.FastestToSlowest)]
+[RankColumn]
+public class DateParserBenchmarks
+{
+    private const string DateTime = "2019-12-13T16:33:06Z";
+
+    private static readonly DateParser _dateParser = new DateParser();
+
+    [Benchmark(Baseline = true)]
+    public void GetYearFromDateTime()
+    {
+        _dateParser.GetYearFromDateTime(DateTime);
+    }
+
+    [Benchmark]
+    public void GetYearFromSplit()
+    {
+        _dateParser.GetYearFromDateTime(DateTime);
+    }
+
+    [Benchmark]
+    public void GetYearFromSubstring()
+    {
+        _dateParser.GetYearFromSubstring(DateTime);
+    }
+
+    [Benchmark]
+    public void GetYearFromSpan()
+    {
+        _dateParser.GetYearFromSpan(DateTime);
+    }
+
+    [Benchmark]
+    public void GetYearFromSpanManualConversion()
+    {
+        _dateParser.GetYearFromSpanManualConversion(DateTime);
+    }
+}
